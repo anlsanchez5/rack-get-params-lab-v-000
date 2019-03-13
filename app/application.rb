f56d30fa466ba73f6f012bd8840aceb3a7a68709
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-13 17:23:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,6 +16,8 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       resp.write cart_search
+    elsif req.path.match(/add/)
+      
     else
       resp.write "Path Not Found"
     end
